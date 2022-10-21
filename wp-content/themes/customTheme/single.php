3b19ad7ca4e9a72d2267e689d4480885b7c64d99
Thuan <quangthuan333@gmail.com>
2022-10-21 15:32:59 +0700

    upload code

--- wp-content/themes/customTheme/single.php
+++ wp-content/themes/customTheme/single.php
@@ -6,16 +6,15 @@
         <div class="col-sm-9">
             <div class="sidebar">
                 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <div class=" pb-4">
+                <div class="border-bottom pb-3 ">
                     <h1 class="fw-semibold text-capitalize">
                         <?php the_title(); ?>
                     </h1>
-                    <?php echo get_the_date(); ?>
+                    <?php echo get_the_date('d/m/Y H:i'); ?>
                 </div>
-
-
-                <?php the_content(); ?>
-
+                <div class="py-3 post-content">
+                    <?php the_content(); ?>
+                </div>
                 <?php endwhile; ?>
                 <?php endif; ?>
             </div>
@@ -27,13 +26,37 @@
             <div class="sidebar">
                 <p class="border-bottom pb-2 fw-bold text-capitalize">Bài Viết Nổi Bật</p>
                 <ol class="list-group">
-                    <a href="#">
+                    <a class="py-2" href="#">
                         <img src="https://cdn.pixabay.com/photo/2016/06/20/13/44/paper-1468883_960_720.jpg"
                             class="card-img-top" alt="...">
-                        <span>10/10/2022</span>
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of the card's content.</p>
+                        <span><?php echo get_the_date('d/m/Y H:i'); ?></span>
+                        <div class="card-body max-lines">
+                            <p class="card-text">
+                                <?php the_title(); ?><?php the_title(); ?><?php the_title(); ?><?php the_title(); ?></p>
+                        </div>
+                    </a><a class="py-2" href="#">
+                        <img src="https://cdn.pixabay.com/photo/2016/06/20/13/44/paper-1468883_960_720.jpg"
+                            class="card-img-top" alt="...">
+                        <span><?php echo get_the_date('d/m/Y H:i'); ?></span>
+                        <div class="card-body max-lines">
+                            <p class="card-text">
+                                <?php the_title(); ?><?php the_title(); ?><?php the_title(); ?><?php the_title(); ?></p>
+                        </div>
+                    </a><a class="py-2" href="#">
+                        <img src="https://cdn.pixabay.com/photo/2016/06/20/13/44/paper-1468883_960_720.jpg"
+                            class="card-img-top" alt="...">
+                        <span><?php echo get_the_date('d/m/Y H:i'); ?></span>
+                        <div class="card-body max-lines">
+                            <p class="card-text">
+                                <?php the_title(); ?><?php the_title(); ?><?php the_title(); ?><?php the_title(); ?></p>
+                        </div>
+                    </a><a class="py-2" href="#">
+                        <img src="https://cdn.pixabay.com/photo/2016/06/20/13/44/paper-1468883_960_720.jpg"
+                            class="card-img-top" alt="...">
+                        <span><?php echo get_the_date('d/m/Y H:i'); ?></span>
+                        <div class="card-body max-lines">
+                            <p class="card-text">
+                                <?php the_title(); ?><?php the_title(); ?><?php the_title(); ?><?php the_title(); ?></p>
                         </div>
                     </a>
             </div>
@@ -41,15 +64,7 @@
         </div>
     </div>
     </div>
-
-
-
 </main>
-
-
-
-
-
 <?php
 	get_footer();
 ?>
